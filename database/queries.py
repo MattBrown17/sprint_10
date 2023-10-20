e35f1f5277cd6860e5246028f5602ff43ee0f597
Matt <matt.brown35022@gmail.com>
2023-10-19 17:02:14 -0700

    Adds a bigger query library for users to connect to python via functions

--- database/queries.py
+++ database/queries.py
@@ -3,3 +3,18 @@ To limit the constants involved in the executed python file: sqlite_example.py
 """
 
 SELECT_CHARACTER_NAME = "SELECT character_id, name FROM charactercreator_character;"
+"""Creates table of character_id and name from charactercreator_character table"""
+
+AVG_ITEM_WEIGHT_PER_CHARACTER = '''
+    SELECT cc_char.name, AVG(ai.weight) AS avg_item_weight
+    FROM charactercreator_character AS cc_char
+    JOIN charactercreator_character_inventory AS cc_inv
+    ON cc_char.character_id = cc_inv.character_id
+    JOIN armory_item AS ai
+    ON ai.item_id = cc_inv.item_id
+    GROUP BY cc_char.character_id
+'''
+"""Creates table of character name and average item weight 
+from charactercreator_character, charactercreator_character_inventory,
+and armory_item tables
+"""
--- database/sqlite_example.py
+++ database/sqlite_example.py
@@ -2,27 +2,43 @@
 The desire is to familiarize myself with SQLite
 """
 
-# Step 0 - import sqlite 3
 import sqlite3
+import pandas as pd
 import queries as q
 
-# Step 1
-# Connect to the Database
-connection = sqlite3.connect('rpg_db.sqlite3')
 
-# Step 2
-# Make a "cursor"
-# Able to interact with the database: safety intermediary
-cursor = connection.cursor()
+def connect_to_db(db_name='rpg_db.sqlite3'):
+    """Connects to a database via sqlite3
 
-# # Step 3
-# # Write a query prompt (SQL Statement)
-# query = 'SELECT character_id, name FROM charactercreator_character;'
+    Parameters
+    ----------
+    db_name : str
+        The database name that is being connected to
+        default value of 'rpg_db.sqlite3'
+    """
+    return sqlite3.connect(db_name)
 
-# Step 4
-# Execute the query on the cursor and fetch us the results
-# "Pulling the results" from the cursor
-results = cursor.execute(q.SELECT_CHARACTER_NAME).fetchall()
+
+def execute_q(connection, query):
+    """Queries the database
+
+    Parameters
+    ----------
+    conn : sqlite3 database connection
+        To know which database is being queried
+    query : str
+        The specific query used to query the database
+    """
+    curs = connection.cursor() # Makes the Cursor
+    curs.execute(query)        # Executes the query
+    return curs.fetchall()
+
 
 if __name__ == '__main__':
-    print(results[:5])
+    conn = connect_to_db()
+    # print(execute_q(conn, q.SELECT_CHARACTER_NAME)[:5])
+    results = execute_q(conn, q.AVG_ITEM_WEIGHT_PER_CHARACTER) # Returns a list of tuples
+    df = pd.DataFrame(results)
+    df.columns = ['name', 'avg_weight']
+    df.to_csv('rpg_db.csv', index= False) # Creates a CSV file in same location
+    print(df.head())
